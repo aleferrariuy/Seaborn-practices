05e1a25fc35203d6351565a828e75b0efa6f876c
aleferrariuy <aleferrari.uy@gmail.com>
2025-09-18 15:47:33 -0300

    code-update

---
+++ code/birth-distribution-year-histogram-v1.0.0.py
@@ -0,0 +1,28 @@
+import seaborn as sns
+import pandas as pd
+
+ruta_archivo_df = 'C:\\Users\\agfep\\Documents\\GitHub\\Seaborn-practices\\code\\datasets\\demo.csv' 
+
+try:
+    df = pd.read_csv(ruta_archivo_df)
+except FileNotFoundError:
+    print(f"Error: El archivo {ruta_archivo_df} no se encontró.")
+    exit()
+
+df['fecha_nacimiento'] = pd.to_datetime(df['fecha_nacimiento'], errors='coerce')
+df['fecha_fallecimiento'] = pd.to_datetime(df['fecha_fallecimiento'], errors='coerce')
+
+# ETL...
+df['anio_nacimiento'] = df['fecha_nacimiento'].dt.year
+df['mes_nacimiento'] = df['fecha_nacimiento'].dt.month
+df['anio_fallecimiento'] = df['fecha_fallecimiento'].dt.year
+df['mes_fallecimiento'] = df['fecha_fallecimiento'].dt.month
+
+# Plotting...
+plt.figure(figsize=(12, 6))
+sns.histplot(data=df, x='anio_nacimiento', kde=True, bins=30)
+plt.title('Distribución de Nacimientos por Año')
+plt.xlabel('Año de Nacimiento')
+plt.ylabel('Número de Nacimientos')
+plt.grid(axis='y', alpha=0.75)
+plt.show()
--- code/primero.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-
-import numpy as np
-import os
-import matplotlib.pyplot as plt
-
-df = pd.read_csv('/code/datasets/demo.csv')
-
